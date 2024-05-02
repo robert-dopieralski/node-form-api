ee3421bee88db58a80cde6a5e967ed7e4ba482c9
Robert Dopieralski <robert@r-solutions.pl>
2024-05-02 11:51:27 +0200

    add example form

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
