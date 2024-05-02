ddb364f5347fbf056d9f6c14f12cc837ab151607
Robert Dopieralski <robert@r-solutions.pl>
2024-05-02 12:16:49 +0200

    add fetching and removal of forms

--- server.js
+++ server.js
@@ -33,7 +33,24 @@ app.post('/api/formularz', (req, res) => {
 
 app.get('/api/formularze', (req, res) => {
     res.status(200).json(formularze);
-  });
+});
+
+app.delete('/api/formularz/:email', (req, res) => {
+    const email = req.params.email;
+  
+    // Znajdź indeks rekordu z danym e-mailem
+    const index = formularze.findIndex((f) => f.email === email);
+  
+    if (index === -1) {
+      // Jeśli e-mail nie istnieje, zwróć błąd 404
+      res.status(404).json({ message: 'Formularz z podanym e-mailem nie istnieje' });
+      return;
+    }
+  
+    formularze.splice(index, 1);
+  
+    res.status(200).json({ message: 'Formularz został usunięty' });
+});
 
 app.listen(port, () => {
   console.log(`Serwer działa na porcie ${port}`);
